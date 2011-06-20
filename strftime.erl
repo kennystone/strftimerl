9fa900a728cfe909769ea7b2c690ef81b0c2f213
Kenny Stone <kennethstone@gmail.com>
2011-06-19 19:56:15 -0500

    %m

--- strftime.erl
+++ strftime.erl
@@ -8,17 +8,21 @@ f({_MegaSec,_Sec,_MicroSec}=Tm, FormatStr) when is_list(FormatStr) ->
   Res = [do_f(Tm, FPart) || FPart <- re:split(FormatStr,"([%][^%])")],
   binary_to_list(list_to_binary(Res)).
 
+do_f(Tm, <<"%m">>) ->
+  {{_YY,MM,_DD},_} = calendar:now_to_local_time(Tm),
+  f2(MM);
+
 do_f(Tm, <<"%y">>) ->
   {{YY,_MM,_DD},_} = calendar:now_to_local_time(Tm),
-  io_lib:format("~2.2.0w",[(YY rem 100)]);
-
+  f2(YY);
 
 do_f(Tm, <<"%D">>) ->
   {{YY, MM, DD},_} = calendar:now_to_local_time(Tm),
-  io_lib:format("~2.2.0w/~2.2.0w/~2.2.0w",[MM,DD,(YY rem 100)]);
+  [f2(MM), "/", f2(DD), "/", f2(YY rem 100)];
 
 do_f(_Tm,Str) -> Str.
 
+f2(N) -> io_lib:format("~2.2.0w",[(N rem 100)]).
 
 % 2011-06-20 00:07:50.46425 UTC
 test_tm() -> {1308,528470,46435}.
@@ -32,5 +36,10 @@ f_D_test() ->
 f_y_test() ->
   ?assertEqual("11", f(test_tm(), "%y")),
   ?assertEqual("fooey11", f(test_tm(), "fooey%y")).
+
+%m test
+f_m_test() ->
+  ?assertEqual("06", f(test_tm(), "%m")),
+  ?assertEqual("fooey06", f(test_tm(), "fooey%m")).
 
 
